ecb55a228b9770baf5d22b2543baaaf68af0d6a5
yasi6969 <santiagocn08@gmail.com>
2025-09-20 15:53:40 -0500

    add max racha

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const preguntaElement = document.getElementById("frase_a_completar");
 const buttonsContainer = document.getElementById("buttons");
 const optionButtons = Array.from(document.querySelectorAll('#buttons button'));
 const btnReiniciar = document.getElementById("btn-reiniciar");
+const rachaLabel = document.querySelector('nav p:nth-child(1)');
+
 
 
 actualizarContadores();
@@ -21,14 +23,19 @@ fetch('ejercicios.json')
   });
 
 function FraseRandom() {
-    preguntaElement.classList.remove("sin-cargar");
+    preguntaElement.classList.add("animacion");
     optionButtons.forEach(btn => btn.classList.remove("incorrecto"));
     habilitarOpciones(true);
     
     const randomIndex = Math.floor(Math.random() * prepositionExercises.length);
     const randomExercise = prepositionExercises[randomIndex];
     
-    preguntaElement.textContent = randomExercise.sentence;
+    setTimeout(() => {
+        preguntaElement.classList.remove("sin-cargar");
+        preguntaElement.textContent = randomExercise.sentence;
+        preguntaElement.classList.remove("animacion");
+    }, 500);
+
     console.log('Frase:', randomExercise.sentence);
     console.log('Respuesta correcta:', randomExercise.answer);
 
@@ -51,7 +58,13 @@ function onOpcionClick(buttonEl) {
     if (respuesta === respuestaCorrecta) {
         buttonEl.classList.add("correcto");
         localStorage.setItem("correctas", parseInt(localStorage.getItem("correctas") || 0) + 1);
-        localStorage.setItem("racha", parseInt(localStorage.getItem("racha") || 0) + 1);
+        const nuevaRacha = parseInt(localStorage.getItem("racha") || 0) + 1;
+        localStorage.setItem("racha", nuevaRacha);
+        const rachaMax = parseInt(localStorage.getItem("racha-max") || 0);
+        if (nuevaRacha > rachaMax) {
+            localStorage.setItem("racha-max", nuevaRacha);
+            actualizarmaxracha()
+        }
         FraseRandom();
 
     } else {
@@ -70,6 +83,10 @@ function habilitarOpciones(habilitar) {
     optionButtons.forEach(btn => btn.disabled = !habilitar);
 }
 
+function actualizarmaxracha() {
+    rachaLabel.setAttribute('data-icon', "Racha Max: 🔥 " + localStorage.getItem("racha-max"));
+}
+actualizarmaxracha()
 
 function actualizarContadores() {
     const rachaElement = document.getElementById("racha");
